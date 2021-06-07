ca3ef80682044fae5332b9c8b25a168b2fc900af
Robert Flores <rrfloresgonzalez@gmail.com>
2021-06-07 12:01:04 -0300

    se implementó una barra de navegación - Robert Flores

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import './components/navbar/navbar';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+return (
+<div className="App">
+  <nav class="navbar navbar-expand-lg navbar-light">
+    <div class="container-fluid">
+      <a className="navbar-brand" href="#">
+        <img src="img/logoNegro.PNG" className="d-inline-block align-text-top" />
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+        aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav d-flex flex-row-reverse bd-highlight">
+          <a class="nav-link" href="#"><img src="img/instagram.png" alt="" id="logoInstagram" /></a>
+          <a class="nav-link" href="#"><img src="img/carrito.png" alt="" /></a>
+          
+        </div>
+      </div>
     </div>
-  );
+  </nav>
+  
+</div>
+);
 }
 
 export default App;
---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,32 @@
+// import React from 'react'
+
+// function navbar (props){
+//   return(
+// <div>
+//     {/* <nav class="navbar navbar-light bg-light">
+//         <div class="container-fluid">
+//             <a class="navbar-brand" href="#">
+//                 <img src="/docs/5.0/assets/brand/bootstrap-logo.svg" alt="" width="30" height="24"
+//                     class="d-inline-block align-text-top" />
+//                 Bootstrap
+//             </a>
+//         </div>
+//     </nav> */}
+//     <h1>SI FUNCIONA</h1>
+// </div>
+// );
+// }
+
+// export default navbar
+
+import React, { Component } from 'react'
+
+export default class navbar extends Component {
+    render() {
+        return (
+            <div>
+                <h1>funciona</h1>
+            </div>
+        )
+    }
+}
